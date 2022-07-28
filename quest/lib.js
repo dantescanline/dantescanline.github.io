499417e9ccf5301462585db4ee9476fbb3b94940
dantescanline <dantescanline@posteo.net>
2022-07-28 17:28:11 +0200

    add inventory tray to rpg

--- quest/lib.js
+++ quest/lib.js
@@ -10,6 +10,47 @@ class V2 {
   }
 }
 
+
+
 function distance(v1, v2) {
   return Math.sqrt(Math.pow(Math.abs(v1.x - v2.x), 2) + Math.pow(Math.abs(v1.y - v2.y), 2))
+}
+
+function angleTo(v1, v2) {
+  return Math.atan2((v2.y - v1.y), (v2.x - v1.x))
+}
+
+class Mob {
+  acceleration = 0.1
+  currentSpeed = 0
+  speed = 5
+
+  constructor(element, xx = 0, yy = 0) {
+    this.element = element
+
+    this.pos = new V2(xx, yy)
+    this.goalPos = V2.from(this.pos)
+    this.updateElement()
+  }
+
+  update() {
+    if (distance(this.pos, this.goalPos) > speed) {
+      if (this.currentSpeed < this.speed) {
+        this.currentSpeed = Math.min(this.speed, this.currentSpeed += this.acceleration)
+      }
+
+      let angle = angleTo(this.pos, this.goalPos)
+
+      this.pos.x += Math.cos(angle) * this.currentSpeed
+      this.pos.y += Math.sin(angle) * this.currentSpeed
+      this.updateElement()
+
+    } else {
+      this.currentSpeed = 0
+    }
+  }
+
+  updateElement() {
+    this.element.style.transform = "translate(" + (this.pos.x) + "px," + (this.pos.y) + "px)";
+  }
 }
--- quest/quest.js
+++ quest/quest.js
@@ -1,29 +1,37 @@
 console.log('hello world')
 
-mainCharacter = document.getElementById('main-character')
+mainCharacterElement = document.getElementById('main-character')
 
 let charPos = new V2(90, 40)
 
 let speed = 3
 
-let goalPos = V2.from(charPos)
+// let goalPos = V2.from(charPos)
 
+let mainChar = new Mob(mainCharacterElement, 80, 150)
 
 window.addEventListener('click', function (e) {
 
-  goalPos.x = e.pageX
-  goalPos.y = e.pageY
+  // goalPos.x = e.pageX
+  // goalPos.y = e.pageY
+  mainChar.goalPos.x = e.pageX
+  mainChar.goalPos.y = e.pageY
 })
 
+
+let mobs = []
+mobs.push(mainChar)
 
 requestAnimationFrame(draw)
 
 
 function draw() {
-  if (distance(charPos, goalPos) > 10) {
-    charPos.x += Math.sign(goalPos.x - charPos.x) * speed
-    charPos.y += Math.sign(goalPos.y - charPos.y) * speed
-  }
-  mainCharacter.style.transform = "translate(" + (charPos.x) + "px," + (charPos.y) + "px)";
+  mobs.map(mob => mob.update())
+  // mainChar.update()
+  // if (distance(charPos, goalPos) > 10) {
+  //   charPos.x += Math.sign(goalPos.x - charPos.x) * speed
+  //   charPos.y += Math.sign(goalPos.y - charPos.y) * speed
+  // }
+  // mainCharacter.style.transform = "translate(" + (charPos.x) + "px," + (charPos.y) + "px)";
   requestAnimationFrame(draw)
 }
